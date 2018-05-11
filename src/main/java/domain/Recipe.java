3d943ada55c1d3a84844eafe34024cde45f90247
Irinej Stjepanović <irinejs@gmail.com>
2018-05-11 12:09:33 +0200

    Setting OneToOne with Enum

--- src/main/java/domain/Recipe.java
+++ src/main/java/domain/Recipe.java
@@ -27,6 +27,9 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     public Long getId() {
         return id;
     }
@@ -107,6 +110,19 @@ public class Recipe {
         this.notes = notes;
     }
 
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
 
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
 
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
 }
