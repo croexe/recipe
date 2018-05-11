d50142e65d592f65bf775b193a1c21662fab0cf3
Irinej Stjepanović <irinejs@gmail.com>
2018-05-11 11:32:48 +0200

    Initial commit

---
+++ src/main/java/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index"})
+    public String getIndexPage(){
+        System.out.println("sta ima 5555555");
+
+        return "index";
+    }
+}
