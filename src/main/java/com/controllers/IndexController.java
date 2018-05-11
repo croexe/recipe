5f2a9c5ea692b9e5e7bc6f789df9829ff451e76e
Irinej Stjepanović <irinejs@gmail.com>
2018-05-11 16:57:40 +0200

    Quering DataBase

--- src/main/java/com/controllers/IndexController.java
+++ src/main/java/com/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package com.controllers;
 
+import com.domain.Category;
+import com.domain.UnitOfMeasure;
+import com.repositories.CategoryRepository;
+import com.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        System.out.println("Category is: " + categoryOptional.get().getId());
+        System.out.println("Category is: " + unitOfMeasureOptional.get().getId());
 
         return "index";
     }
