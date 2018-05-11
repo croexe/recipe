5cc4461bb28f77e7f06da7c768b279ae9e6331c9
Irinej Stjepanović <irinejs@gmail.com>
2018-05-11 19:41:26 +0200

    Setting up Recipe Service

--- src/main/java/com/controllers/IndexController.java
+++ src/main/java/com/controllers/IndexController.java
@@ -1,34 +1,22 @@
 package com.controllers;
 
-import com.domain.Category;
-import com.domain.UnitOfMeasure;
-import com.repositories.CategoryRepository;
-import com.repositories.UnitOfMeasureRepository;
+import com.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"","/","index"})
-    public String getIndexPage(){
-
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
-
-        System.out.println("Category is: " + categoryOptional.get().getId());
-        System.out.println("Category is: " + unitOfMeasureOptional.get().getId());
-
+    public String getIndexPage(Model model){
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
---
+++ src/main/java/com/services/RecipeService.java
@@ -0,0 +1,11 @@
+package com.services;
+
+import com.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+
+}
