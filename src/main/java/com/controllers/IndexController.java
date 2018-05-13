2ecdafa078d0117143ad26edef832f22fd2cc540
Irinej Stjepanović <irinejs@gmail.com>
2018-05-13 21:09:57 +0200

    Importing Bootstrap WebJars

--- src/main/java/com/controllers/IndexController.java
+++ src/main/java/com/controllers/IndexController.java
@@ -1,10 +1,12 @@
 package com.controllers;
 
 import com.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -14,9 +16,12 @@ public class IndexController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping({"","/","index"})
-    public String getIndexPage(Model model){
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
+        log.debug("Getting Index page");
+
         model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
