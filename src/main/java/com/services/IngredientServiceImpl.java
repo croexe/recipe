9cc1792345ced6715767438919ea35d05e9fd829
Irinej Stjepanović <irinejs@gmail.com>
2018-05-16 18:00:56 +0200

    Updating Ingredient

--- src/main/java/com/services/IngredientServiceImpl.java
+++ src/main/java/com/services/IngredientServiceImpl.java
@@ -1,11 +1,15 @@
 package com.services;
 
 import com.commands.IngredientCommand;
+import com.converters.IngredientCommandToIngredient;
 import com.converters.IngredientToIngredientCommand;
+import com.domain.Ingredient;
 import com.domain.Recipe;
 import com.repositories.RecipeRepository;
+import com.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -15,10 +19,14 @@ public class IngredientServiceImpl implements IngredientService {
 
 private final IngredientToIngredientCommand ingredientToIngredientCommand;
 private final RecipeRepository recipeRepository;
+private final UnitOfMeasureRepository unitOfMeasureRepository;
+private final IngredientCommandToIngredient ingredientCommandToIngredient;
 
-    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository) {
+    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository, IngredientCommandToIngredient ingredientCommandToIngredient) {
         this.ingredientToIngredientCommand = ingredientToIngredientCommand;
         this.recipeRepository = recipeRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.ingredientCommandToIngredient = ingredientCommandToIngredient;
     }
 
     @Override
@@ -42,5 +50,44 @@ private final RecipeRepository recipeRepository;
         }
 
         return ingredientCommandOptional.get();
+    }
+
+    @Override
+    @Transactional
+    public IngredientCommand saveIngredientCommand(IngredientCommand command) {
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(command.getRecipeId());
+
+        if(!recipeOptional.isPresent()){
+            log.error("Recipe not found for id: " + command.getRecipeId());
+            return new IngredientCommand();
+        } else {
+            Recipe recipe = recipeOptional.get();
+
+            Optional<Ingredient> ingredientOptional = recipe
+                    .getIngredients()
+                    .stream()
+                    .filter(ingredient -> ingredient.getId().equals(command.getId()))
+                    .findFirst();
+
+            if(ingredientOptional.isPresent()){
+                Ingredient ingredientFound = ingredientOptional.get();
+                ingredientFound.setDescription(command.getDescription());
+                ingredientFound.setAmount(command.getAmount());
+                ingredientFound.setUom(unitOfMeasureRepository
+                        .findById(command.getUom().getId())
+                        .orElseThrow(() -> new RuntimeException("UOM NOT FOUND")));
+            } else {
+                recipe.addIngredient(ingredientCommandToIngredient.convert(command));
+            }
+            //add new Ingredient
+            Recipe savedRecipe = recipeRepository.save(recipe);
+
+            return ingredientToIngredientCommand.convert(savedRecipe.getIngredients().stream()
+                    .filter(recipeIngredients -> recipeIngredients.getId().equals(command.getId()))
+                    .findFirst()
+                    .get());
+        }
+
     }
 }
---
+++ src/main/java/com/services/UnitOfMeasureService.java
@@ -0,0 +1,9 @@
+package com.services;
+
+import com.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+public interface UnitOfMeasureService {
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
