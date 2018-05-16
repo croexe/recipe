815eef67afaf003ef586475bb50c6d8abdac1cf5
Irinej Stjepanović <irinejs@gmail.com>
2018-05-16 18:05:45 +0200

    IngredientController quick Fix

--- src/main/java/com/controllers/IngredientController.java
+++ src/main/java/com/controllers/IngredientController.java
@@ -47,7 +47,7 @@ public class IngredientController {
                                        @PathVariable String id, Model model){
         model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
 
-        model.addAttribute("uomlist", unitOfMeasureService.listAllUoms() );
+        model.addAttribute("uomList", unitOfMeasureService.listAllUoms() );
         return "recipe/ingredient/ingredientform";
     }
 
