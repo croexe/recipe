dbff88a7dcadd910061b6e43eae1c49c8c806208
Irinej Stjepanović <irinejs@gmail.com>
2018-05-17 19:41:16 +0200

    Setting up Front End for image Upload

--- src/main/java/com/services/ImageServiceImpl.java
+++ src/main/java/com/services/ImageServiceImpl.java
@@ -1,14 +1,45 @@
 package com.services;
 
+import com.domain.Recipe;
+import com.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
-    log.debug("Filed uploaded!");
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for(byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+        } catch (IOException e){
+            log.error("Error found", e);
+            e.printStackTrace();
+        }
+
     }
 }
