d1f3c231196d8a97aabcfaf831e6a5cba5504f60
Irinej Stjepanović <irinejs@gmail.com>
2018-05-18 15:36:32 +0200

    Setting up Global Exception Handler for 400 error

--- src/main/java/com/controllers/RecipeController.java
+++ src/main/java/com/controllers/RecipeController.java
@@ -70,17 +70,6 @@ public class RecipeController {
         return modelAndView;
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handlerNumberFormat(Exception exception){
 
-        log.error("Handling Bad request exception");
-        log.error(exception.getMessage());
-        ModelAndView modelAndView = new ModelAndView();
-
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception", exception);
-        return modelAndView;
-    }
 
 }
